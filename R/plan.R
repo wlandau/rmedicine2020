b5a595c8eb8d024452183764835eda2d69a266a2
wlandau-lilly <will.landau@lilly.com>
2020-06-16 22:15:55 -0400

    Update plan

--- R/plan.R
+++ R/plan.R
@@ -1,28 +1,28 @@
 plan <- drake_plan(
   sim = target(
-    seq_len(1e3),
+    seq_len(10),
     hpc = FALSE
   ),
   patients = target(
     simulate_trial(
-      mean_control = 15,
-      mean_treatment = 10,
-      patients_per_arm = patients_per_arm,
+      mean_control = 20,
+      mean_treatment = mean_treatment,
+      patients_per_arm = 100,
       censor = 30
     ),
     dynamic = map(sim),
-    transform = map(patients_per_arm = c(100, 200, 300, 400)),
+    transform = map(mean_treatment = c(10, 15, 20)),
     format = "fst_tbl"
   ),
   models = target(
-    model_hazard(patients, 2e3),
+    model_hazard(patients, 50),
     dynamic = map(patients),
-    transform = map(patients, .id = patients_per_arm),
+    transform = map(patients, .id = mean_treatment),
     format = "fst_tbl"
   ),
   summaries = target(
     summarize_models(models),
-    transform = map(models, .id = patients_per_arm),
+    transform = map(models, .id = mean_treatment),
     format = "fst_tbl"
   ),
   results = target(
