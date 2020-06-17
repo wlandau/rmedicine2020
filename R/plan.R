ef3aef3dbd23649b978f9220764eda4024e33951
wlandau-lilly <will.landau@lilly.com>
2020-06-16 22:19:41 -0400

    Scale back up

--- R/plan.R
+++ R/plan.R
@@ -1,6 +1,6 @@
 plan <- drake_plan(
   sim = target(
-    seq_len(10),
+    seq_len(1e3),
     hpc = FALSE
   ),
   patients = target(
@@ -15,7 +15,7 @@ plan <- drake_plan(
     format = "fst_tbl"
   ),
   models = target(
-    model_hazard(patients, 50),
+    model_hazard(patients, 2e3),
     dynamic = map(patients),
     transform = map(patients, .id = mean_treatment),
     format = "fst_tbl"
